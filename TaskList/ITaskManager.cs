40026144db4bbc060ab1773b8b98d08ee9100424
patrickeikema1998 <48601026+patrickeikema1998@users.noreply.github.com>
2025-01-25 15:17:41 +0100

    First commit at #3

---
+++ TaskList/ITaskManager.cs
@@ -0,0 +1,12 @@
+﻿namespace TaskList.Core
+{
+    public interface ITaskManager
+    {
+        void AddTask(string project, string description);
+        Task GetTask(int taskId);
+        void CheckTask(int taskId);
+        void UncheckTask(int taskId);
+        List<Task> GetTasksOfToday();
+        Dictionary<DateTime, List<Task>> GetTasksByDeadline();
+    }
+}
---
+++ TaskList/Task.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+
+namespace TaskList
+{
+	public class Task
+	{
+		public long Id { get; set; }
+
+		public string Description { get; set; }
+
+		public bool Done { get; set; }
+
+		public DateTime Deadline { get; set; }
+	}
+}
